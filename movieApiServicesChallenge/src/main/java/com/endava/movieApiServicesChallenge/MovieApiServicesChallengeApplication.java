aa0ea82ac363f204a5c3868809a55729a91b8593
Juan Felipe Marin Florez <jumarin@javeriana.edu.co>
2021-09-11 14:34:53 -0500

    spring boot app created

---
+++ movieApiServicesChallenge/src/main/java/com/endava/movieApiServicesChallenge/MovieApiServicesChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.endava.movieApiServicesChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApiServicesChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApiServicesChallengeApplication.class, args);
+	}
+
+}
---
+++ movieApiServicesChallenge/src/test/java/com/endava/movieApiServicesChallenge/MovieApiServicesChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.movieApiServicesChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiServicesChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
